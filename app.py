488e714772fd348c08413c65f205dfa084506656
Riya Kushwaha <riyakushwaha1125@gmail.com>
2025-06-16 21:18:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def login():
     username = st.text_input("Username")
     password = st.text_input("Password", type="password")
     if st.button("Login"):
-        if username == "User" and password == "User@123":
+        if username == "Riya" and password == "Riya@123":
             st.session_state["authenticated"] = True
         else:
             st.error("Invalid username or password")
